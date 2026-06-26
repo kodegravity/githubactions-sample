a8d5e1e757620f97de1377cdad9f29330a3dc633
kodegravity <info@kodegravity.com>
2026-06-25 21:09:36 -0400

    added initial github workflow

---
+++ src/test/java/io/codeqube/kodegravitygithubactions/KodegravityGithubactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codeqube.kodegravitygithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodegravityGithubactionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
