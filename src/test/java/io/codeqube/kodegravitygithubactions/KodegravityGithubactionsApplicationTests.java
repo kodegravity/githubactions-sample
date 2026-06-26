5b2f29f6cc10c6f7596e945c8e925da1dd602a4b
kodegravity <info@kodegravity.com>
2026-06-25 21:24:23 -0400

    updated pipeline

--- src/test/java/io/codeqube/kodegravitygithubactions/KodegravityGithubactionsApplicationTests.java
+++ src/test/java/io/codeqube/kodegravitygithubactions/KodegravityGithubactionsApplicationTests.java
@@ -3,11 +3,14 @@ package io.codeqube.kodegravitygithubactions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 class KodegravityGithubactionsApplicationTests {
 
     @Test
     void contextLoads() {
     }
+
 
 }
